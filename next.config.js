bb3e895f3435c07c0ddacd44c9891e4cd3dd362d
sandybradley <sandy.bradley@gmail.com>
2024-07-08 16:50:04 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -59,7 +59,27 @@
 
 // file: next.config.js
 
-module.exports = {
+// module.exports = {
+//   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
+
+module.exports = nextConfig;
+
 
